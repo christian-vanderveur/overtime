a28f7f67a6c3d9c381d24ea495713180d0da2cde
Christian <christian.vanderveur@gmail.com>
2018-02-21 22:58:59 -0700

    made tabs more usable and refactered

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,13 +2,13 @@
 FactoryGirl.define do
   factory :post do
     date Date.today
-    rational "Some Rational"
-
+    rational "Some Rationale"
+    user
   end
 
   factory :second_post, class: "Post" do
     date Date.yesterday
     rational "Some more content"
-
+    user
   end
 end
