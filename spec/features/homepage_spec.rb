a11dc11ce72fc84d5736ddf7084835ef5896a3ad
Christian <christian.vanderveur@gmail.com>
2018-03-08 13:59:51 -0700

    building mailer functionality

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -14,8 +14,8 @@ describe 'Homepage' do
   end
 
   it 'allows the employee to change the audit log status from the homepage' do
-    audit_log = FactoryGirl.create(:audit_log)
-    user = FactoryGirl.create(:user)
+    audit_log = FactoryBot.create(:audit_log)
+    user = FactoryBot.create(:user)
     login_as(user, :scope => :user)
 
     audit_log.update(user_id: user.id)
